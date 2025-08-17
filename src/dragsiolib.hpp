94e390f3a8ddad784002442a83d080448ad6f8f0
Corey-Burdick <coreyburdickwork@gmail.com>
2025-08-17 16:30:03 -0400

    Added timer functionality in a class called "Timer" in "dragsiolib.hpp", and implemented a toggleable timer mode in "main.cpp", which displays generation and sort times in ms.

--- src/dragsiolib.hpp
+++ src/dragsiolib.hpp
@@ -3,5 +3,26 @@
 #include <chrono>
 #include <random>
 
+class Timer {
+public:
+  
+  Timer() : start_time(std::chrono::high_resolution_clock::now()) {}
+
+  void reset() {
+    start_time = std::chrono::high_resolution_clock::now();
+  }
+
+  long long elapsed_milliseconds() const {
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+    return duration.count();
+  }
+
+private:
+
+  std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
+
+};
+
 void printIntVector(std::vector<int> inputVector, bool verbose);
 std::vector<int> generateRandomVector(int size);
--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,22 @@
 
 int main(int argc, char* argv[]) {
   
+  bool timerMode = true;
   bool verboseMode = true;
   int mainSize = 20;
+
+  Timer my_Timer;
   std::vector<int> mainVector = generateRandomVector(mainSize);
 
+  if (timerMode) printf("Time to Generate: %lldms\n", my_Timer.elapsed_milliseconds());
   printf("Generated Array: ");
   printIntVector(mainVector, verboseMode);
+  printf("\n");
 
+  if (timerMode) my_Timer.reset();
   bubbleSort(mainVector);
 
+  if (timerMode) printf("Time to Sort: %lldms\n", my_Timer.elapsed_milliseconds());
   printf("Sorted Array: ");
   printIntVector(mainVector, verboseMode);
 
