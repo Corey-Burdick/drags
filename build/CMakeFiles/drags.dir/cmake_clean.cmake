a029a3754e6ff11195d186f13b67991dc8176ea4
Corey-Burdick <coreyburdickwork@gmail.com>
2025-08-19 17:23:52 -0400

    Added more functionality for arguments in main.cpp

---
+++ build/CMakeFiles/drags.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/drags.dir/link.d"
+  "CMakeFiles/drags.dir/src/dragsiolib.cpp.o"
+  "CMakeFiles/drags.dir/src/dragsiolib.cpp.o.d"
+  "CMakeFiles/drags.dir/src/dragsortlib.cpp.o"
+  "CMakeFiles/drags.dir/src/dragsortlib.cpp.o.d"
+  "CMakeFiles/drags.dir/src/main.cpp.o"
+  "CMakeFiles/drags.dir/src/main.cpp.o.d"
+  "drags"
+  "drags.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drags.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
